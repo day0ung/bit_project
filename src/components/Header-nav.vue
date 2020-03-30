0a55709aa743d8e90ffe85bf02f04d5172ecb9c8
WooHyun Nam <>
2020-03-30 19:30:30 +0900

    헤더 수정

--- src/components/Header-nav.vue
+++ src/components/Header-nav.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="Header-nav">
         <div id="nav" style="display: table; margin: auto;">
-            <router-link to="/home">Home</router-link> |
+            <router-link to="/home">제발 되어야 합니다!!!!!!!!!!</router-link> |
             <router-link to="/aboutus">About Us</router-link> |
             <router-link to="/private">개인스터디</router-link> |
             <router-link to="/group">그룹스터디</router-link> |
