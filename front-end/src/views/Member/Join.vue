eacf0ec979a1e91e97a468829a6447f2c27448dd
JeongDaYoung <jdy9436@naver.com>
2020-04-07 09:54:58 +0900

    Member폴더만듬, memberstroe, memberrouter

--- front-end/src/views/Member/Join.vue
+++ front-end/src/views/Member/Join.vue
@@ -1,12 +1,32 @@
 <template>
     <div>
         <h1>회원가입 페이지</h1>
+          <button id="show-modal" @click="showModal">Show Modal</button>
+          <Modal v-if="showModal" @close="showModal = false">
+        <!--
+        you can use custom content here to overwrite
+        default content
+        -->
+           <h3 slot="header">custom header</h3>
+        </Modal>
+    
     </div>
 </template>
 
 <script>
+// import Modal from "@/views/Member/modal.vue";
 export default {
-
+    // data(){
+    //     showModal: false
+    // },
+    // components:{
+    //     Modal
+    // },
+    // methods:{
+    //     showModal(){
+    //         showModal: true
+    //     }
+    // }
 }
 </script>
 
