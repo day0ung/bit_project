a4e2400cd577995e9b3d9112002303ead91571d8
JeongDaYoung <jdy9436@naver.com>
2020-04-07 11:44:02 +0900

    회원가입하다맘

--- front-end/src/views/Member/Join.vue
+++ front-end/src/views/Member/Join.vue
@@ -1,32 +1,37 @@
 <template>
     <div>
         <h1>회원가입 페이지</h1>
-          <button id="show-modal" @click="showModal">Show Modal</button>
-          <Modal v-if="showModal" @close="showModal = false">
-        <!--
-        you can use custom content here to overwrite
-        default content
-        -->
-           <h3 slot="header">custom header</h3>
-        </Modal>
-    
+        <div class="row_group">
+                    <div class="join_row">
+                        <h3 class="join_title"><label for="id">아이디</label></h3>
+                        <span class="ps_box int_id">
+							<input type="text" id="id" name="id" class="int" title="ID" maxlength="20">
+                        </span>
+                        <span class="error_next_box" id="idMsg" style="display:none" aria-live="assertive"></span>
+                    </div>
+
+                    <div class="join_row">
+                        <h3 class="join_title"><label for="pswd1">비밀번호</label></h3>
+                        <span class="ps_box int_pass" id="pswd1Img">
+							<input type="password" id="pswd1" name="pswd1" class="int" title="비밀번호 입력" aria-describedby="pswd1Msg" maxlength="20">
+                            <span class="lbl"><span id="pswd1Span" class="step_txt"></span></span>
+						</span>
+                        <span class="error_next_box" id="pswd1Msg" style="display:none" aria-live="assertive">5~12자의 영문 소문자, 숫자와 특수기호(_)만 사용 가능합니다.</span>
+
+                        <h3 class="join_title"><label for="pswd2">비밀번호 재확인</label></h3>
+                        <span class="ps_box int_pass_check" id="pswd2Img">
+							<input type="password" id="pswd2" name="pswd2" class="int" title="비밀번호 재확인 입력" aria-describedby="pswd2Blind" maxlength="20">
+							<span id="pswd2Blind" class="wa_blind">설정하려는 비밀번호가 맞는지 확인하기 위해 다시 입력 해주세요.</span>
+						</span>
+                        <span class="error_next_box" id="pswd2Msg" style="display:none" aria-live="assertive"></span>
+                    </div>
+                </div>
     </div>
 </template>
 
 <script>
-// import Modal from "@/views/Member/modal.vue";
 export default {
-    // data(){
-    //     showModal: false
-    // },
-    // components:{
-    //     Modal
-    // },
-    // methods:{
-    //     showModal(){
-    //         showModal: true
-    //     }
-    // }
+
 }
 </script>
 
