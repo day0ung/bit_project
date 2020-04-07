7065cc815e9aa99cd70a2ff6cead5473b2729f41
jongha-shin <smilejohn@hanmail.net>
2020-04-07 12:31:14 +0900

    group info with name

--- back-end/palette/src/main/java/com/palette/model/GroupDto.java
+++ back-end/palette/src/main/java/com/palette/model/GroupDto.java
@@ -24,8 +24,11 @@ public class GroupDto implements Serializable {
 
 	private int groupInfoSeq;	// group_info_seq;
 	private int memberSeq;
+	
 	private int interBigSeq;
+	
 	private int interSmallSeq;
+	
 	private String groupName;
 	private String info;
 	private int maxMember;
@@ -36,15 +39,19 @@ public class GroupDto implements Serializable {
 
 	private GroupSchedule groupSchedule;
 	
+	private InterBigDto interBigDto;
+	private InterSmallDto interSmallDto;
+	private MemberDto memberDto;
+	
 	
 	public GroupDto() {
 	
 	}
 
-	
 
 	public GroupDto(int groupInfoSeq, int memberSeq, int interBigSeq, int interSmallSeq, String groupName, String info,
-			int maxMember, String image, String startDate, String endDate, int del, GroupSchedule groupSchedule) {
+			int maxMember, String image, String startDate, String endDate, int del, GroupSchedule groupSchedule,
+			InterBigDto interBigDto, InterSmallDto interSmallDto, MemberDto memberDto) {
 		super();
 		this.groupInfoSeq = groupInfoSeq;
 		this.memberSeq = memberSeq;
@@ -58,6 +65,9 @@ public class GroupDto implements Serializable {
 		this.endDate = endDate;
 		this.del = del;
 		this.groupSchedule = groupSchedule;
+		this.interBigDto = interBigDto;
+		this.interSmallDto = interSmallDto;
+		this.memberDto = memberDto;
 	}
 
 
@@ -66,7 +76,8 @@ public class GroupDto implements Serializable {
 		return "GroupDto [groupInfoSeq=" + groupInfoSeq + ", memberSeq=" + memberSeq + ", interBigSeq=" + interBigSeq
 				+ ", interSmallSeq=" + interSmallSeq + ", groupName=" + groupName + ", info=" + info + ", maxMember="
 				+ maxMember + ", image=" + image + ", startDate=" + startDate + ", endDate=" + endDate + ", del=" + del
-				+ ", groupSchedule=" + groupSchedule + "]";
+				+ ", groupSchedule=" + groupSchedule + ", interBigDto=" + interBigDto + ", interSmallDto="
+				+ interSmallDto + ", memberDto=" + memberDto + "]";
 	}
 
 
@@ -74,97 +85,151 @@ public class GroupDto implements Serializable {
 		return groupInfoSeq;
 	}
 
+
 	public void setGroupInfoSeq(int groupInfoSeq) {
 		this.groupInfoSeq = groupInfoSeq;
 	}
+
 
 	public int getMemberSeq() {
 		return memberSeq;
 	}
 
+
 	public void setMemberSeq(int memberSeq) {
 		this.memberSeq = memberSeq;
 	}
+
 
 	public int getInterBigSeq() {
 		return interBigSeq;
 	}
 
+
 	public void setInterBigSeq(int interBigSeq) {
 		this.interBigSeq = interBigSeq;
 	}
+
 
 	public int getInterSmallSeq() {
 		return interSmallSeq;
 	}
 
+
 	public void setInterSmallSeq(int interSmallSeq) {
 		this.interSmallSeq = interSmallSeq;
 	}
+
 
 	public String getGroupName() {
 		return groupName;
 	}
 
+
 	public void setGroupName(String groupName) {
 		this.groupName = groupName;
 	}
+
 
 	public String getInfo() {
 		return info;
 	}
 
+
 	public void setInfo(String info) {
 		this.info = info;
 	}
+
 
 	public int getMaxMember() {
 		return maxMember;
 	}
 
+
 	public void setMaxMember(int maxMember) {
 		this.maxMember = maxMember;
 	}
+
 
 	public String getImage() {
 		return image;
 	}
 
+
 	public void setImage(String image) {
 		this.image = image;
 	}
+
 
 	public String getStartDate() {
 		return startDate;
 	}
 
+
 	public void setStartDate(String startDate) {
 		this.startDate = startDate;
 	}
+
 
 	public String getEndDate() {
 		return endDate;
 	}
 
+
 	public void setEndDate(String endDate) {
 		this.endDate = endDate;
 	}
+
 
 	public int getDel() {
 		return del;
 	}
 
+
 	public void setDel(int del) {
 		this.del = del;
 	}
+
 
 	public GroupSchedule getGroupSchedule() {
 		return groupSchedule;
 	}
 
+
 	public void setGroupSchedule(GroupSchedule groupSchedule) {
 		this.groupSchedule = groupSchedule;
 	}
 
+
+	public InterBigDto getInterBigDto() {
+		return interBigDto;
+	}
+
+
+	public void setInterBigDto(InterBigDto interBigDto) {
+		this.interBigDto = interBigDto;
+	}
+
+
+	public InterSmallDto getInterSmallDto() {
+		return interSmallDto;
+	}
+
+
+	public void setInterSmallDto(InterSmallDto interSmallDto) {
+		this.interSmallDto = interSmallDto;
+	}
+
+
+	public MemberDto getMemberDto() {
+		return memberDto;
+	}
+
+
+	public void setMemberDto(MemberDto memberDto) {
+		this.memberDto = memberDto;
+	}
+
+	
 	
 }
