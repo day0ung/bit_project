63693e3491c86eca37ef283caa7b70b536c3d08c
JeongDaYoung <jdy9436@naver.com>
2020-04-08 18:08:24 +0900

    css파일 따로빼고, 회원가입 바꿈

--- front-end/src/views/Group/menu/detail2.vue
+++ front-end/src/views/Group/menu/detail2.vue
@@ -14,18 +14,58 @@
         <li @click="gotoDetail(one.boardSeq)">{{one.title}}</li>
         <li>{{one.memberSeq}}</li>
       </ul>
-    </div> 
+    </div>
+
+    <el-table
+      :data="tableData"
+      stripe
+      style="width: 100%"
+      row-click="gotoClick"
+      >
+      <el-table-column
+        prop="boardSeq"
+        label="글번호"
+        width="200px">
+      </el-table-column>
+      <el-table-column
+        prop="title"
+        label="글제목"
+        width="500px"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="memberSeq"
+        label="작성자"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="readCount"
+        label="조회수"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="writeDate"
+        label="작성일"
+        >
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
-<script>
+<script scoped>
+import 'element-ui/lib/theme-chalk/index.css';
+
 export default {
   data(){
     return{
-      boardList: []
+      boardList: [],
+      tableData: []
     }
   },
   methods:{
+    gotoClick(){
+      alert("확인")
+    },
     gotoDetail(seq){
       this.$router.push({
         path : "/group/detail2/depth2/" + seq
@@ -37,16 +77,25 @@ export default {
       axios.get("http://localhost:9000/groupBoardList")
                 .then(res => {
             this.boardList = res.data
+            this.tableData = res.data
           })
   }
 }
 </script>
 
 <style scoped>
-  .li_table {width: 700px; margin: auto;}
-  .li_table ul {clear: left;margin: 0;padding: 0;list-style-type: none;} 
-  /* list-style-type 블릿표시 padding: 0; 들여쓰기*/
-  .li_table .subject {font-weight: bold;text-align: center;}
-  .li_table ul li {text-align: center;float: left;margin: 0;padding: 2px, 1px;width: 40%;}
-  .li_table ul .col {width: 15%;} /* 표 라인 맞추기기 위해*/
+.hr{
+  margin: auto;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  background: #9e9e9e;
+  height: 1px;
+  width: 90%;
+}
+.li_table {width: 700px; margin: auto;}
+.li_table ul {clear: left;margin: 0;padding: 0;list-style-type: none;} 
+/* list-style-type 블릿표시 padding: 0; 들여쓰기*/
+.li_table .subject {font-weight: bold;text-align: center;}
+.li_table ul li {text-align: center;float: left;margin: 0;padding: 2px, 1px;width: 40%;height: 35px;}
+.li_table ul .col {width: 15%;} /* 표 라인 맞추기기 위해*/
 </style>
--- front-end/src/views/Group/menu/detail4.vue
+++ front-end/src/views/Group/menu/detail4.vue
@@ -4,11 +4,109 @@
       <h5>detail4</h5>
       <br>
       <h6>{{ this.$store.state.s_subStore.data }}</h6>
+
+      <input type="date">
+
+      <div class="block">
+    <span class="demonstration">Default</span>
+    <el-date-picker
+      v-model="value1"
+      type="daterange"
+      range-separator="To"
+      start-placeholder="Start date"
+      end-placeholder="End date">
+    </el-date-picker>
+  </div>
+  <div class="block">
+    <span class="demonstration">With quick options</span>
+    <el-date-picker
+      v-model="value2"
+      type="daterange"
+      align="right"
+      unlink-panels
+      range-separator="To"
+      start-placeholder="Start date"
+      end-placeholder="End date"
+      :picker-options="pickerOptions">
+    </el-date-picker>
+  </div>
+
+  <el-row>
+  <el-button>Default</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
+</el-row>
+
+<el-row>
+  <el-button plain>Plain</el-button>
+  <el-button type="primary" plain>Primary</el-button>
+  <el-button type="success" plain>Success</el-button>
+  <el-button type="info" plain>Info</el-button>
+  <el-button type="warning" plain>Warning</el-button>
+  <el-button type="danger" plain>Danger</el-button>
+</el-row>
+
+<el-row>
+  <el-button round>Round</el-button>
+  <el-button type="primary" round>Primary</el-button>
+  <el-button type="success" round>Success</el-button>
+  <el-button type="info" round>Info</el-button>
+  <el-button type="warning" round>Warning</el-button>
+  <el-button type="danger" round>Danger</el-button>
+</el-row>
+
+<el-row>
+  <el-button icon="el-icon-search" circle></el-button>
+  <el-button type="primary" icon="el-icon-edit" circle></el-button>
+  <el-button type="success" icon="el-icon-check" circle></el-button>
+  <el-button type="info" icon="el-icon-message" circle></el-button>
+  <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+  <el-button type="danger" icon="el-icon-delete" circle></el-button>
+</el-row>
+
   </div>
 </template>
 
-<script>
+<script scoped>
+import 'element-ui/lib/theme-chalk/index.css';
+
 export default {
+    data() {
+    return {
+      pickerOptions: {
+        shortcuts: [{
+          text: 'Last week',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
+          text: 'Last month',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
+          text: 'Last 3 months',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+            picker.$emit('pick', [start, end]);
+          }
+        }]
+      },
+      value1: '',
+      value2: ''
+    };
+  },
   mounted(){
     this.$store.state.currpage = this.$route.path
   }
