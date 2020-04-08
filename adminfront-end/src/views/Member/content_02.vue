1e8dfe23b5a8a009d88f98f57d0e6058ee316e71
bit_jonghyun <goste390@gmail.com>
2020-04-08 11:13:21 +0900

    Modify basic structure of administrator page

---
+++ adminfront-end/src/views/Member/content_02.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="content02_view">
+        <h1> manager member22222222222222222221122222222222222222222222221</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title:'hello'
+        }
+    },
+    methods:{
+
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
