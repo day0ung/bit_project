8eef7285585f32e364bb612196b431a0520b6b23
jongha-shin <smilejohn@hanmail.net>
2020-04-10 18:48:51 +0900

    head top update - check plz

--- front-end/src/components/header/Header-top.vue
+++ front-end/src/components/header/Header-top.vue
@@ -1,66 +1,66 @@
 <template>
-<div>
-    <div class="header_top">
-<!-- <<<<<<< HEAD
-        <div class="header_component">
+    <div>
+        <div class="header_top">
+    <!-- <<<<<<< HEAD
+            <div class="header_component">
+                <div class="searchWrap">
+                    <form name="serach" method="get" action="#">
+                        <input type="text" maxlength="20" name="mainSearchWord" placeholder="검색어를 입력해주세요">
+                        <el-button icon="el-icon-search" size="small" circle></el-button>
+                    </form>
+    ======= -->
             <div class="searchWrap">
                 <form name="serach" method="get" action="#">
                     <input type="text" maxlength="20" name="mainSearchWord" placeholder="검색어를 입력해주세요">
                     <el-button icon="el-icon-search" size="small" circle></el-button>
                 </form>
-======= -->
-        <div class="searchWrap">
-            <form name="serach" method="get" action="#">
-                <input type="text" maxlength="20" name="mainSearchWord" placeholder="검색어를 입력해주세요">
-                <el-button icon="el-icon-search" size="small" circle></el-button>
-            </form>
-        </div>
-        <div class="inner">
-            <div v-if="isLogin" style="margin-right: 30px;">
+            </div>
+            <div class="inner">
+                <div v-if="isLogin" style="margin-right: 30px;">
+                        <span>
+                            <button  @click="showModal = true">로그인</button>
+                            <Modal v-if="showModal" @close="showModal = false">
+                            </Modal>
+                        </span> |
+                        <span><button @click="$router.push({name:'join'})">회원가입</button></span>
+                </div>
+                <div v-else style="margin-right: 30px;">
                     <span>
-                        <button  @click="showModal = true">로그인</button>
+                        <!-- <p>{{loginUser.memberId}}님 환영합니다</p> -->
+                        <button @click="$router.push ({name:'mypage'})">마이페이지</button>
+                    </span> |
+                    <span><button @click="logout">로그아웃</button></span>
+    <!-- >>>>>>> origin/NewDayoung -->
+                </div>
+                <div class="logo">
+                    <router-link to="/">
+    <!-- <<<<<<< HEAD
+                        <img style="height: 55px; margin-top: -10px;" src='../../assets/css/images/logo.png' alt="팔레트" />
+                    </router-link>
+                </div>
+                <div class="member">
+                    <span>
+                        <button id="show-modal" @click="showModal = true">로그인</button>
                         <Modal v-if="showModal" @close="showModal = false">
+                    
+                        you can use custom content here to overwrite
+                        default contentzz
+                    
                         </Modal>
                     </span> |
-                    <span><button @click="$router.push({name:'join'})">회원가입</button></span>
+                    <span><button @click="$router.push({name:'join'})">회원가입</button></span> |
+                    <span><button @click="logOut">로그아웃</button></span>
+                </div>
+    ======= -->
+                        <img style="height: 55px; margin-top: -25px;" src='../../assets/css/images/logo.png' alt="팔레트" />
+                    </router-link>
+                    
+                    
+    <!-- >>>>>>> origin/NewDayoung -->
+                </div>
             </div>
-            <div v-else style="margin-right: 30px;">
-                <span>
-                    <!-- <p>{{loginUser.memberId}}님 환영합니다</p> -->
-                    <button @click="$router.push ({name:'mypage'})">마이페이지</button>
-                </span> |
-                <span><button @click="logout">로그아웃</button></span>
-<!-- >>>>>>> origin/NewDayoung -->
-            </div>
-            <div class="logo">
-                <router-link to="/">
-<!-- <<<<<<< HEAD
-                    <img style="height: 55px; margin-top: -10px;" src='../../assets/css/images/logo.png' alt="팔레트" />
-                </router-link>
-            </div>
-            <div class="member">
-                <span>
-                    <button id="show-modal" @click="showModal = true">로그인</button>
-                    <Modal v-if="showModal" @close="showModal = false">
-                   
-                    you can use custom content here to overwrite
-                    default contentzz
-                   
-                    </Modal>
-                </span> |
-                <span><button @click="$router.push({name:'join'})">회원가입</button></span> |
-                <span><button @click="logOut">로그아웃</button></span>
-            </div>
-======= -->
-				    <img style="margin-left:-50px" src='../../assets/css/images/logo.png' alt="팔레트" />
-			    </router-link>
-                 
-            	
-<!-- >>>>>>> origin/NewDayoung -->
         </div>
     </div>
-</div>
-
 </template>
 
 <script>
