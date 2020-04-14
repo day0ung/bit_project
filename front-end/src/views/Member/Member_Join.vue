67aa667ad485e9d0bef2834cc0902abb85c8e9a8
JeongDaYoung <jdy9436@naver.com>
2020-04-14 10:05:31 +0900

    회원가입 수정

--- front-end/src/views/Member/Member_Join.vue
+++ front-end/src/views/Member/Member_Join.vue
@@ -1,207 +1,155 @@
 <template>
-<div>
-<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
-  <div id="ndls">
-	<p class="tar mb10"><span class="red">*</span> 표시가 된 곳은 필수 항목입니다.</p>
-	
-	<div class="ndls_bwrite join_write">
-		<div class="box w100">
-			<ul>
-		
-				<li>
-					<dl>
-						<dt><span class="red">*</span> <label for="user_id">아이디</label></dt>
-						<dd class="pw_wrap">
-							<button class="ndls_btn2 gray" style="text-decoration:none;" 
-							@click="idcheck">
-							아이디 중복확인</button>
-							<span class="text1">※ 영문, 숫자 혼용 8자리이상</span>
-							<br>
-							<p class="text1">※ 중복확인버튼을 눌러주세요</p>
-						</dd>
-					</dl>
-				</li>
-				<li>
-					<dl>
-						<dt><span class="red">*</span> <label for="user_pwd">비밀번호</label></dt>
-						<dd class="pw_wrap">
-							<el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
-						</dd>
-					</dl>
-				</li>
-				<li>
-					<dl>
-						<dt><span class="red">*</span> <label for="user_pwd_chk">비밀번호확인</label></dt>
-						<dd>
-						    <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
-						</dd>
-					</dl>
-				</li>
-				<li>
-					<dl>
-						<dt><label for="user_pwd_chk">E-MAIL</label></dt>
-						<dd>
-						</dd>
-					</dl>
-				</li>
-				<li>
-					<dl>
-						<dt><span class="red">*</span> <label for="manage_code">나이</label></dt>
-						<dd>
-							<el-select v-model="ruleForm.region" placeholder="나이대를 선택해주세요">
-								<el-option label="10대" value="10대"></el-option>
-								<el-option label="20-24" value="20-24"></el-option>
-								<el-option label="25-29" value="25-29"></el-option>
-								<el-option label="30-34" value="30-34"></el-option>
-								<el-option label="40대" value="40대"></el-option>
-							</el-select>
-						</dd>
-					</dl>
-				</li>
-				<li>
-					<dl>
-						<dt><span class="red">*</span> <label for="exchange_mobile">핸드폰</label></dt>
-						<dd>
-						</dd>
-					</dl>
-				</li>
-
-				
-				<li>
-					<dl>
-						<dt><label for="radioLable">성별</label></dt>
-						<dd>
-							  <el-radio v-model="radio1" label="1" border>남자</el-radio>
-  							  <el-radio v-model="radio1" label="2" border>여자</el-radio>
-						</dd>
-					</dl>
-				</li>
-				
-				<li>
-					<dl>
-						<dt><span class="red">*</span> <label for="zipcode">주소</label></dt>
-						<dd class="col3">
-							<input type="text" class="ndls_input mb5" v-model="postcode" style="width:40%;" maxlength="5" placeholder="우편번호" readonly="readonly">
-							<input type="button" @click="execDaumPostcode" value="우편번호 찾기" class="ndls_btn2 gray" style=""><br>
-							<input type="text" class="ndls_input" v-model="address" style="width:80%; margin-bottom:5px;" placeholder="주소" readonly="readonly">
-							<input type="text" class="ndls_input" v-model="extraAddress" ref="extraAddress" style="width:80%;" placeholder="상세주소">
-						</dd>
-					</dl>
-				</li>
-				<li>
-					<dl>
-						<dt><span class="red">*</span> <label for="zipcode">관심지역</label></dt>
-						<dd class="col3">
-						</dd>
-					</dl>
-				</li>
-				<li>
-					<dl>
-						<dt><span class="red">*</span> <label for="zipcode">관심분야</label></dt>
-						<dd class="col3">
-							<div class="radios">
-							</div>
-						</dd>
-					</dl>
-				</li>
-				<li>
-					<dl>
-						<dt><span class="red">*</span> <label for="zipcode">상세분야</label></dt>
-						<dd class="col3">
-							
-						</dd>
-					</dl>
-				</li>
-			</ul>
-		</div>
-	</div>
-	
-		<p class="btn_w">
+    <div class="create">
+        <div class="createContainer">
+            <div class="title">그룹 스터디 생성</div>
+            <el-form :model="ruleForm" label-position="top" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
+                <!-- 아이디 설정 -->
+                <el-form-item label="아이디" prop="memberId">
+                    <el-input placeholder="아이디를 입력해 주세요" v-model="ruleForm.memberId"></el-input>
+                </el-form-item>
+				<!-- 비밀번호 -->
+				 <el-form-item label="비밀번호" prop="pass">
+					<el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+				</el-form-item>
+				<el-form-item label="비밀번호 확인" prop="checkPass">
+					<el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+				</el-form-item>
+				<!--  이메일 -->
+				 <el-form-item prop="email" label="Email">
+					<el-input v-model="ruleForm.email"></el-input>
+				 </el-form-item>	 
+                <!--  나이대선택 -->
+				<el-form-item label="성별/ 연령" prop="age">
+					<el-select v-model="ruleForm.age" placeholder="나이대를 선택해주세요">
+						<el-option label="10대" value="10대"></el-option>
+						<el-option label="20-24" value="20-24"></el-option>
+						<el-option label="25-29" value="25-29"></el-option>
+						<el-option label="30-34" value="30-34"></el-option>
+						<el-option label="40대" value="40대"></el-option>
+					</el-select>
+					||
+					<el-radio v-model="ruleForm.gender" label="1" >남자</el-radio>
+  					<el-radio v-model="ruleForm.gender" label="2" >여자</el-radio>
+				</el-form-item>
 			
-		</p>
-	</div>
-</el-form>
-</div>
+            
+               
+                <!-- 서브밋버튼 -->
+                <div class="submitBtnDiv">
+                    <el-form-item>
+                        <el-button type="primary" @click="submitForm('ruleForm')">Create</el-button>
+                        <el-button @click="resetForm('ruleForm')">Reset</el-button>
+                    </el-form-item>
+                </div>
+            </el-form>
+        </div>
+
+    </div>
 </template>
 
 <script>
-import dayoung from '@/assets/css/member/memberJoin.css'
-
 export default {
-	data(){
-		return{
-		postcode: '',
-      	address: '',
-     	extraAddress: '',
-		pickerOptions: {
-          disabledDate(time) {
-            return time.getTime() > Date.now();
-          },
-          shortcuts: [{
-            text: 'Today',
-            onClick(picker) {
-              picker.$emit('pick', new Date());
-            }
-          }, {
-            text: 'Yesterday',
-            onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24);
-              picker.$emit('pick', date);
-            }
-          }, {
-            text: 'A week ago',
-            onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
-              picker.$emit('pick', date);
-            }
-          }]
-		},
-		
-		ruleForm: {
-          name: '',
-          region: '',
-          date1: '',
-		  date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-		},
-		radio1: '1',
-		rules: {
-          name: [
-            { required: true, message: 'Please input Activity name', trigger: 'blur' },
-            { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' }
-          ],
-          region: [
-            { required: true, message: 'Please select Activity zone', trigger: 'change' }
-          ],
-          date1: [
-            { type: 'date', required: true, message: 'Please pick a date', trigger: 'change' }
-          ],
-          date2: [
-            { type: 'date', required: true, message: 'Please pick a time', trigger: 'change' }
-          ],
-          type: [
-            { type: 'array', required: true, message: 'Please select at least one activity type', trigger: 'change' }
-          ],
-          resource: [
-            { required: true, message: 'Please select activity resource', trigger: 'change' }
-          ],
-          desc: [
-            { required: true, message: 'Please input activity form', trigger: 'blur' }
-          ]
+    data(){
+		var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('비밀번호를 입력해주세요'));
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
         }
-	  }
-		
-	},
-	
-	  methods: {
-		execDaumPostcode() {
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('Please input the password again'));
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('비밀번호가 일치하지 않습니다'));
+        } else {
+          callback();
+        }
+      };
+        return{
+            ruleForm: {
+				memberId: '',
+				pass: '',
+				checkPass: '',
+				email: '', 
+				gender: '',
+				age: '',
+				postcode: '',
+      			address: '',
+     			extraAddress: '',
+                maxMember:'',
+                schedeul: [],
+                date: '',
+                desc: '',
+                dialogImageUrl: '',
+                dialogVisible: false
+            },
+            rules: {
+            memberId: [
+                { required: true, message: '아이디를 입력해주세요', trigger: 'blur' },
+                { min: 3, max: 20, message: '스터디 그룹명은 3글자에서 20자로 사이로 지어주세요', trigger: 'blur' }
+			],
+			pass: [
+            	{ required: true, validator: validatePass, trigger: 'blur' }
+			],
+			checkPass: [
+				{ required: true, validator: validatePass2, trigger: 'blur' }
+			],
+			email: [
+				{ required: true, message: 'Please input email address', trigger: 'blur' },
+				{ type: 'email', message: 'Please input correct email address', trigger: ['blur', 'change'] }
+			],
+			age: [
+				{ required: true, message: '나이대를 선택해 주세요', trigger: 'blur' },
+			],
+			gender:[
 
-		new daum.Postcode({
+			],
+			postcode:[
+
+			],
+			address:[
+
+			],
+			extraAddress:[
+
+			],
+            schedeul: [
+                { type: 'array', required: true, message: '주간일정을 선택해주세요', trigger: 'change' }
+            ],
+            date: [
+                { type: 'date', required: true, message: '시작일과 종료일을 선택해주세요', trigger: 'change' }
+            ],
+            desc: [
+                { required: true, message: '스터디 그룹을 소개해주세요', trigger: 'blur' },
+                { min: 1, max: 500, message: '그룹소개는 500자 이내로 작성해주세요.', trigger: 'blur' }
+            ]
+            },
+            bigInterList: "",
+            smallInterList: "",
+            bigValue: 0,
+            InterListAll: ''
+            }
+    },
+    methods:{
+        submitForm(formName) {
+            this.$refs[formName].validate((valid) => {
+            if (valid) {
+                alert('submit!');
+            } else {
+                console.log('error submit!!');
+                return false;
+            }
+            });
+        },
+        resetForm(formName) {
+            this.$refs[formName].resetFields();
+		},
+		execDaumPostcode() {
+			new daum.Postcode({
 			onComplete: (data) => {
 			if (data.userSelectedType === 'R') {
 				this.address = data.roadAddress;
@@ -226,13 +174,32 @@ export default {
 			}
 			},
 		}).open();
-	
-	},
-	idcheck(){
-		alert(this.memberId)
-		
-	},
+		}
+    }
 
-  }
 }
 </script>
+
+<style scope>
+
+.createContainer {
+    position: relative;
+    margin: auto;
+    overflow: hidden;
+    width: 600px;
+    height: auto;
+    border-radius: 10px;
+}
+.title{
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+    font-size: 18px;
+    margin: auto;
+    margin-top: 30px;
+    margin-bottom: 20px;
+    text-align: center;
+}
+.submitBtnDiv{
+    margin-left: -120px;
+    text-align: center;
+}
+</style>
