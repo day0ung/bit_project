77397651d09b61a5ee7991f55cd2ba2605c3ea25
bit_jonghyun <goste390@gmail.com>
2020-04-14 17:51:03 +0900

    adminfront  table listup

--- adminfront-end/src/views/Member/content_02.vue
+++ adminfront-end/src/views/Member/content_02.vue
@@ -1,8 +1,165 @@
 <template>
     <div class="content02_view">
-        <h1> manager member22222222222222222221122222222222222222222222221</h1>
+        <h1> Company member Management tool </h1>
+      <pre>
+-> callList
+/adminCompanyMemberShowAll
+/showCompanyInfo  (Memberdto)
+/adminGetCompanyInfoOne
+/finishApprovalCompanyList
+/adminGetCompanyLocationALl
+/adminGetCompanyNmaeAll
+      </pre>
 
-    <el-table
+      <el-table 
+        :data="CompanyMemberAll"
+        stripe
+        style="width:100$">
+
+            <el-table-column
+              prop="memberSeq"
+              label="memberSeq"
+              width="100">
+            </el-table-column>
+
+            <el-table-column
+              prop="memberId"
+              label="memberId"
+              width="100">
+            </el-table-column>
+
+            <el-table-column
+              prop="pwd"
+              label="pwd"
+              width="100">
+            </el-table-column>
+
+            <el-table-column
+              prop="memberName"
+              label="memberName"
+              width="100">
+            </el-table-column>
+            <el-table-column
+              prop="birth"
+              label="birth"
+              width="100">
+            </el-table-column>
+
+            <el-table-column
+              prop="address"
+              label="address"
+              width="100">
+            </el-table-column>
+
+            <el-table-column
+              prop="location"
+              label="location"
+              width="100">
+            </el-table-column>
+
+            <el-table-column
+              prop="email"
+              label="email"
+              width="100">
+            </el-table-column>
+
+            <el-table-column
+              prop="myMoney"
+              label="myMoney"
+              width="100">
+            </el-table-column>
+
+            <el-table-column
+              prop="cv"
+              label="cv"
+              width="100">
+            </el-table-column>
+
+            <el-table-column
+              prop="companyInfo"
+              label="companyInfo"
+              width="100">
+            </el-table-column>
+            <el-table-column
+              prop="companyLogo"
+              label="companyLogo"
+              width="100">
+            </el-table-column>
+
+            <el-table-column
+              prop="auth"
+              label="auth"
+              width="100">
+            </el-table-column>
+
+            <el-table-column
+              prop="del"
+              label="del"
+              width="100">
+            </el-table-column>
+            <el-table-column
+              prop="dislike"
+              label="dislike"
+              width="100">
+            </el-table-column>
+
+
+
+      </el-table>
+
+      <br> <hr>
+      <!-- <button v-on:click="showAdminCompanyMamberShowAll"> Call Company List All </button> -->
+      <ul>
+        <li v-for="item in CompanyMemberAll" :key="item.memberSeq">{{item}}</li>
+
+      </ul>
+
+
+      
+
+
+    </div>
+</template>
+
+<script scoped>
+
+import 'element-ui/lib/theme-chalk/display.css';
+const path = "http://localhost:9000/";
+
+export default {
+    data(){
+      return{
+        CompanyMemberAll:[]
+
+      }
+        
+    },
+    created(){
+      
+        axios.get(path+"adminCompanyMemberShowAll")
+        .then(res=>{
+          console.log(res)
+          this.CompanyMemberAll=res.data
+        })
+      
+    },
+
+    methods:{
+      
+        
+          
+     
+    }
+
+}
+</script>
+
+<style>
+
+</style>
+
+
+    <!-- <el-table
       :data="tableData"
       style="width: 100%">
       <el-table-column
@@ -19,49 +176,4 @@
         prop="address"
         label="Address">
       </el-table-column>
-    </el-table>
-
-    </div>
-</template>
-
-<script scoped>
-
-import 'element-ui/lib/theme-chalk/display.css';
-
-
-export default {
-    data(){
-        return {
-       
-             tableData: [{
-            date: '2016-05-03',
-            name: 'Tom',
-            address: 'No. 189, Grove St, Los Angeles'
-          }, {
-            date: '2016-05-02',
-            name: 'Tom',
-            address: 'No. 189, Grove St, Los Angeles'
-          }, {
-            date: '2016-05-04',
-            name: 'Tom',
-            address: 'No. 189, Grove St, Los Angeles'
-          }, {
-            date: '2016-05-01',
-            name: 'Tom',
-            address: 'No. 189, Grove St, Los Angeles'
-          }]
-
-
-        }
-    },
-    methods:{
-
-
-    }
-
-}
-</script>
-
-<style>
-
-</style>
+    </el-table> -->
