e2775d166919d416c9d801644c1b8cdcccc84743
bit_jonghyun <goste390@gmail.com>
2020-04-14 10:50:01 +0900

    Create admin backend class file

---
+++ back-end/palette/src/main/java/com/palette/controller/Admin_CompanyMemController.java
@@ -0,0 +1,18 @@
+package com.palette.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.palette.service.MemberService;
+import com.palette.service.adminMemberService;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class Admin_CompanyMemController {
+	@Autowired
+	adminMemberService adminservice;
+    
+    
+
+}//end of Admin_CompanyMemController class
