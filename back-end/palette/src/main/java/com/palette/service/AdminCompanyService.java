1d166445190cb3c305cb6bbb0935f2e919fd3d16
bit_jonghyun <goste390@gmail.com>
2020-04-14 16:04:47 +0900

    Backend processing for company member

---
+++ back-end/palette/src/main/java/com/palette/service/AdminCompanyService.java
@@ -0,0 +1,51 @@
+package com.palette.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.palette.dao.adminCompanyDao;
+import com.palette.model.MemberDto;
+
+
+@Service
+@Transactional
+public class AdminCompanyService {
+	@Autowired
+	adminCompanyDao adminmemberdao;
+	
+	public List<MemberDto> adminGetCompanyMemberListAll(){
+		return adminmemberdao.adminGetCompanyMemberListAll();
+	}
+	
+	public MemberDto adminGetCompanyInfoOne(MemberDto dto) {
+		
+		return adminmemberdao.adminGetCompanyInfoOne(dto);
+	}
+	
+	public MemberDto approvalrequestCompanyList(){
+		return adminmemberdao.approvalrequestCompanyList();
+	}
+	
+	public MemberDto finishApprovalCompanyList() {
+		return adminmemberdao.finishApprovalCompanyList();
+	}
+	
+	public List<String> adminGetCompanyLocationALl(){
+		return adminmemberdao.adminGetCompanyLocationALl();
+		
+	}
+	
+	public List<String> adminGetCompanyNmaeAll(){
+		return adminmemberdao.adminGetCompanyNmaeAll();
+	}
+
+	
+	
+	
+	
+	
+
+}// end of adminCompanyService class
