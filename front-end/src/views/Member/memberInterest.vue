c742154bba8f6081d24eb5670ae1a29f4a85c8c9
JeongDaYoung <jdy9436@naver.com>
2020-04-14 18:02:07 +0900

    회원가입완료

--- front-end/src/views/Member/memberInterest.vue
+++ front-end/src/views/Member/memberInterest.vue
@@ -1,14 +1,5 @@
 <template>
-  <div class="checks"> 
-	  <input type="checkbox" id="ex_chk"> <label for="ex_chk">체크박스</label> 
   
-	<div class="radios">
-		<input type="radio" id="ex_rd" name="ex_rds" > <label for="ex_rd">라디오버튼</label> 
-		<br>
-		<input type="radio" id="ex_rd2" name="ex_rds"> <label for="ex_rd2">라디오버튼</label> 
-	</div>
-  </div>
-	
 </template>
 
 <script>
@@ -18,109 +9,194 @@ export default {
 </script>
 
 <style>
-.checks {position: relative;} 
-.checks input[type="checkbox"] {
-	 /* 실제 체크박스는 화면에서 숨김 */ 
-	 position: absolute; 
-	 width: 1px; 
-	 height: 1px;
-	 padding: 0; 
-	 margin: -1px; 
-	 overflow: hidden; 
-	 clip:rect(0,0,0,0); 
-	 border: 0 
-} 
-.checks input[type="checkbox"] + label { 
-	display: inline-block; 
-	position: relative; 
-	cursor: pointer; 
-	-webkit-user-select: none; 
-	-moz-user-select: none; 
-	-ms-user-select: none; 
-} 
-.checks input[type="checkbox"] + label:before {
-	 /* 가짜 체크박스 */ 
-	 content: ' ';
-     display: inline-block; 
-	 width: 26px;
-	 height: 26px;  
-	 line-height: 21px; /* 세로정렬을 위해 높이값과 일치 */ 
-	 margin: -2px 8px 0 0; 
-	 text-align: center; 
-	 vertical-align: middle; 
-	 background: #fafafa; 
-	 border: 1px solid #cacece; 
-	 border-radius : 3px; 
-	 box-shadow: 0px 1px 2px rgba(0,0,0,0.05), inset 0px -15px 10px -12px rgba(0,0,0,0.05); 
-} 
-.checks input[type="checkbox"] + label:active:before, .checks input[type="checkbox"]:checked + label:active:before { 
-	 box-shadow: 0 1px 2px rgba(0,0,0,0.05), inset 0px 1px 3px rgba(0,0,0,0.1); 
 
-} 
-.checks input[type="checkbox"]:checked + label:before { /* 체크박스를 체크했을때 */ 
-	 content: '\2714 '; /* 체크표시 유니코드 사용 */ 
-	 color: #fff;
-	 width: 26px;
-	 height: 26px; 
-	 text-shadow: 1px 1px #fff; 
-	 background: #ef6c00; 
-	 border-color: #ef6c00; 
-	 box-shadow: 0px 1px 2px rgba(0,0,0,0.05), inset 0px -15px 10px -12px rgba(0,0,0,0.05), inset 15px 10px -12px rgba(255,255,255,0.1); 
+</style><template>
+    <div class="create">
+        <div class="createContainer">
+            <div class="title">회원가입이 완료되었습니다. 관심목록을 설정해 주세요</div>
+            <el-form :model="ruleForm" label-position="top" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
+               <!-- 관심지역 -->
+               <el-form-item label="관심지역" prop="interArea">
+                <p style="color: #ef6c00; margin-top:-30px " >서울 / 경기 / 인천</p>
+                    <el-checkbox-group v-model="ruleForm.interArea">
+                    <el-checkbox label="강남" name="type"></el-checkbox>
+                    <el-checkbox label="홍대" name="type"></el-checkbox>
+                    <el-checkbox label="종각" name="type"></el-checkbox>
+                    <el-checkbox label="의정부" name="type"></el-checkbox>
+                    <el-checkbox label="분당" name="type"></el-checkbox>
+                    <el-checkbox label="안양" name="type"></el-checkbox>
+                    <el-checkbox label="신촌" name="type"></el-checkbox>
+                    <el-checkbox label="인천" name="type"></el-checkbox>
+                    <el-checkbox label="부형" name="type"></el-checkbox>
+                    <el-checkbox label="노원" name="type"></el-checkbox>
+                    <el-checkbox label="건대" name="type"></el-checkbox>
+                    <el-checkbox label="수원" name="type"></el-checkbox>
+                    <el-checkbox label="일산" name="type"></el-checkbox>
+                </el-checkbox-group>
+               </el-form-item>
+
+               <!-- 관심분야 -->
+               <el-form-item label="관심분야" prop="interBig">
+                    <el-checkbox-group v-model="ruleForm.interBig" style="float: left">
+                        <el-checkbox label="대학생/취업" :value="1" name="type" ></el-checkbox>
+                        <el-checkbox label="공무원/임용" name="type" ></el-checkbox>
+                        <el-checkbox label="어학/회화" name="type" ></el-checkbox>
+                        <el-checkbox label="라이프/취미" name="type" ></el-checkbox>
+                </el-checkbox-group>
+               </el-form-item>
+
+
+                <!-- 상세분야 -->
+               <el-form-item label="상세분야" prop="interSmall" >
+                  <p style="color: #82CF1E; margin-bottom:-10px" >대학생/취업</p>
+                   <div style="border-bottom: solid 1px; color: #cecece; padding-bottom: 10px">
+                    <el-checkbox-group v-model="ruleForm.interSmall" size="medium">
+                        <el-checkbox label="공기업" name="type" ></el-checkbox>
+                        <el-checkbox label="자격증" name="type"></el-checkbox>
+                        <el-checkbox label="자소서" name="type"></el-checkbox>
+                        <el-checkbox label="경영/사무" name="type"></el-checkbox>
+                        <el-checkbox label="디자인" name="type"></el-checkbox>
+                        <el-checkbox label="전문직" name="type"></el-checkbox>
+                        <el-checkbox label="IT/Internet" name="type"></el-checkbox>
+                        <el-checkbox label="유통/무역" name="type"></el-checkbox>
+                        <el-checkbox label="서비스" name="type"></el-checkbox>
+                    </el-checkbox-group> 
+                   </div>
+                     <p style="color: #82CF1E; margin-bottom:-10px" >공무원/임용</p>
+                   <div style="border-bottom: solid 1px; color: #cecece; padding-bottom: 10px">
+                    <el-checkbox-group v-model="ruleForm.interSmall" size="medium">
+                        <el-checkbox label="일반행정" name="type" ></el-checkbox>
+                        <el-checkbox label="세무" name="type"></el-checkbox>
+                        <el-checkbox label="교육행정" name="type"></el-checkbox>
+                        <el-checkbox label="교정" name="type"></el-checkbox>
+                        <el-checkbox label="군무원" name="type"></el-checkbox>
+                        <el-checkbox label="사회복지" name="type"></el-checkbox>
+                        <el-checkbox label="검찰사무" name="type"></el-checkbox>
+                        <el-checkbox label="소방" name="type"></el-checkbox>
+                        <el-checkbox label="경찰" name="type"></el-checkbox>
+                    </el-checkbox-group>
+                     </div> 
+                     <p style="color: #82CF1E; margin-bottom:-10px" >어학/회화</p>
+                   <div style="border-bottom: solid 1px; color: #cecece; padding-bottom: 10px">
+                    <el-checkbox-group v-model="ruleForm.interSmall" size="medium">
+                        <el-checkbox label="토익" name="type" ></el-checkbox>
+                        <el-checkbox label="토스/오픽" name="type"></el-checkbox>
+                        <el-checkbox label="기초영어회화" name="type"></el-checkbox>
+                        <el-checkbox label="일본어" name="type"></el-checkbox>
+                        <el-checkbox label="중국어" name="type"></el-checkbox>
+                        <el-checkbox label="제2 외국어" name="type"></el-checkbox>
+                    </el-checkbox-group>
+                     </div> 
+                     <p style="color: #82CF1E; margin-bottom:-10px" >라이프/취미</p>
+                   <div style="border-bottom: solid 1px; color: #cecece; padding-bottom: 10px">
+                    <el-checkbox-group v-model="ruleForm.interSmall" size="medium">
+                        <el-checkbox label="여행/아웃도어" name="type" ></el-checkbox>
+                        <el-checkbox label="운동" name="type"></el-checkbox>
+                        <el-checkbox label="봉사활동" name="type"></el-checkbox>
+                        <el-checkbox label="공예/만들기" name="type"></el-checkbox>
+                        <el-checkbox label="댄스/무용" name="type"></el-checkbox>
+                        <el-checkbox label="요리/제조" name="type"></el-checkbox>
+                        <el-checkbox label="자유" name="type"></el-checkbox>
+                    </el-checkbox-group>
+                     </div> 
+               </el-form-item>
+
+                <!-- 서브밋버튼 -->
+                <div class="submitBtnDiv">
+                    <el-form-item>
+                        <el-button type="primary" @click="submitForm('ruleForm')">회원가입</el-button>
+                        <el-button @click="resetForm('ruleForm')">취소</el-button>
+                    </el-form-item>
+                </div>
+            </el-form>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+		var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('비밀번호를 입력해주세요'));
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('비밀번호 확인을 입력해주세요'));
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('비밀번호가 일치하지 않습니다'));
+        } else {
+          callback();
+        }
+      };
+        return{
+            ruleForm: {
+                interArea:[],
+                interBig: [],
+                interSmall:[]
+            },
+            rules: {
+     
+                interArea:[
+                    { required: true, message: '관심지역을 선택해주세요', trigger: 'change' },
+                ],
+                interBig:[
+                    { required: true, message: '관심분야를 선택해주세요', trigger: 'change' },
+                ],
+                interSmall:[
+                    { required: true, message: '상세분야를 선택해주세요', trigger: 'change' },
+                ]
+            },
+           
+        }
+    },
+    methods:{
+        submitForm(formName) {
+            this.$refs[formName].validate((valid) => {
+            if (valid) {
+                alert(this.ruleForm.interBig);
+            } else {
+                console.log('error submit!!');
+                return false;
+            }
+            });
+        },
+        resetForm(formName) {
+            this.$refs[formName].resetFields();
+		},
+
+    }
+
 }
+</script>
 
+<style scope>
 
-
-/* 라디오버튼 */
-.radios input[type="radio"] { 
-	position: absolute;
-	 width: 1px; 
-	 height: 1px; 
-	 padding: 0; 
-	 margin: -1px; 
-	 overflow: hidden; 
-	 clip:rect(0,0,0,0); border: 0; 
+.createContainer {
+    position: relative;
+    margin: auto;
+    overflow: hidden;
+    width: 600px;
+    height: auto;
+    border-radius: 10px;
 }
-.radios input[type="radio"] + label { 
-	display: inline-block; 
-	position: relative; 
-	padding-left: 30px; 
-	cursor: pointer; 
-	-webkit-user-select: none; 
-	-moz-user-select: none; 
-	-ms-user-select: none; 
+.title{
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+    font-size: 18px;
+    margin: auto;
+    margin-top: 30px;
+    margin-bottom: 20px;
+    text-align: center;
 }
-.radios input[type="radio"] + label:before { 
- content: ' ';
-     display: inline-block; 
-	 width: 26px;
-	 height: 26px;  
-	 line-height: 21px; /* 세로정렬을 위해 높이값과 일치 */ 
-	 margin: -2px 8px 0 0; 
-	 text-align: center; 
-	 vertical-align: middle; 
-	 background: #fafafa; 
-	 border: 1px solid #cacece; 
-	 border-radius : 3px; 
-	 box-shadow: 0px 1px 2px rgba(0,0,0,0.05), inset 0px -15px 10px -12px rgba(0,0,0,0.05); 
-	}
-.radios input[type="radio"] + label:active:before, .radios input[type="radio"]:checked + label:active:before { 
-	box-shadow: 0 1px 2px rgba(0,0,0,0.05), inset 0px 1px 3px rgba(0,0,0,0.1); }
-.radios input[type="radio"]:checked + label:before { 
- 	content: '\2714 '; /* 체크표시 유니코드 사용 */ 
-	 color: #fff;
-	 width: 26px;
-	 height: 26px; 
-	 text-shadow: 1px 1px #fff; 
-	 background: #ef6c00; 
-	 border-color: #ef6c00; 
-	 box-shadow: 0px 1px 2px rgba(0,0,0,0.05), inset 0px -15px 10px -12px rgba(0,0,0,0.05), inset 15px 10px -12px rgba(255,255,255,0.1);
+.submitBtnDiv{
+    margin-left: -120px;
+    text-align: center;
 }
-.radios input[type="radio"]:checked + label:after {
-	content: ''; 
-	position: absolute; 
-	background: #99a1a7; 
-	box-shadow: inset 0px 0px 10px rgba(0,0,0,0.3); 
-}
-
-
 </style>
