3e5a921914415d8ef01632aeb3e674c448e1d468
bit_jonghyun <goste390@gmail.com>
2020-04-16 17:05:56 +0900

    change group tap structure

--- adminfront-end/src/router/r_Group/r_Group.js
+++ adminfront-end/src/router/r_Group/r_Group.js
@@ -3,11 +3,33 @@ export default[{
     name:'Group',
     component:() => import('@/views/Group/group.vue'),
     children:[
+  
         {
-            path:'/search',
-            name:'search',
-            component:() => import('@/views/Group/search.vue'),
-        }
+            path:'/',
+            name:'acceptlist',
+            component:() => import('@/views/Accept/content_00.vue'),
+        },
+        {
+            path:'/group01',
+            name:'acceptlist1',
+            component:() => import('@/views/Accept/content_01.vue'),
+        },
+        {
+            path:'/group02',
+            name:'acceptlist2',
+            component:() => import('@/views/Accept/content_02.vue'),
+        },
+        {
+            path:'/group03',
+            name:'acceptlist3',
+            component:() => import('@/views/Accept/content_03.vue'),
+        },
+        {
+            path:'/group04',
+            name:'acceptlist4',
+            component:() => import('@/views/Accept/content_04.vue'),
+        },
+
     ]
 
 }]
---
+++ adminfront-end/src/views/Group/content_02.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="content02_view">
+        <h1> {{title}} group 22222222222222222221122222222222222222222222221</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title:'group2'
+        }
+    },
+    methods:{
+
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
