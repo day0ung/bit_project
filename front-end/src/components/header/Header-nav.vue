a8b8dc50f5895a3a7453572160d7cb25b02c85e3
JeongDaYoung <jdy9436@naver.com>
2020-04-20 10:00:19 +0900

    마이페이지 수정중. 메인 css변경

--- front-end/src/components/header/Header-nav.vue
+++ front-end/src/components/header/Header-nav.vue
@@ -66,10 +66,10 @@ export default {
 
 .headerBar{
     line-height: 30px;
-    height: 8px;
-    background: #ff5151;
+    height: 1px;
+    background: #d6d6d6;
     margin: auto calc(0% - 50vw); /* container 무시하고 전체 적용 */
     text-align: center;
-}
+} 
 
 </style>
