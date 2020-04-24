887cf0bdf1a81673d5df7922fa5c401b4a29e903
bit_jonghyun <goste390@gmail.com>
2020-04-24 17:49:18 +0900

    [admin frot ] memeber List

--- adminfront-end/src/views/Member/content_01.vue
+++ adminfront-end/src/views/Member/content_01.vue
@@ -5,103 +5,106 @@
         :data="showAllMember"
         stripe
         style="width: 100%">
-        
+                    
             <el-table-column
-                prop="memberSeq"
-                label="memberSeq"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="memberId"
-                label="memberId"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="pwd"
-                label="pwd"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="memberName"
-                label="memberName"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="address"
-                label="address"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="interArea"
-                label="interArea"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop=""
-                label=""
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="email"
-                label="email"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="age"
-                label="age"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="gender"
-                label="gender"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="myMoney"
-                label="myMoney"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="cv"
-                label="cv"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="companyInfo"
-                label="companyInfo"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="companyLogo"
-                label="companyLogo"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="auth"
-                label="auth"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="del"
-                label="del"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="dislike"
-                label="dislike"
-                width="100">
-            </el-table-column>
-            <el-table-column
-                prop="memberInterDto"
-                label="memberInterDto"
-                width="100">
+            prop="memberSeq"
+            label="memberSeq"
+            width="100">
             </el-table-column>
 
             <el-table-column
-                prop="interBigDtos"
-                label="interBigDtos"
-                width="100">
+            prop="memberId"
+            label="memberId"
+            width="100">
             </el-table-column>
+
+            <el-table-column
+            prop="pwd"
+            label="pwd"
+            width="100">
+            </el-table-column>
+
+            <el-table-column
+            prop="memberName"
+            label="memberName"
+            width="100">
+            </el-table-column>
+
+            <el-table-column
+            prop="address"
+            label="address"
+            width="100">
+            </el-table-column>
+
+            <el-table-column
+            prop="interArea"
+            label="interArea"
+            width="100">
+            </el-table-column>
+
+            <el-table-column
+            prop="email"
+            label="email"
+            width="100">
+            </el-table-column>
+
+            <el-table-column
+            prop="age"
+            label="age"
+            width="100">
+            </el-table-column>
+
+            <el-table-column
+            prop="gender"
+            label="gender"
+            width="100">
+            </el-table-column>
+
+            <el-table-column
+            prop="myMoney"
+            label="myMoney"
+            width="100">
+            </el-table-column>
+
+            <el-table-column
+            prop="cv"
+            label="cv"
+            width="100">
+            </el-table-column>
+
+            <el-table-column
+            prop="auth"
+            label="auth"
+            width="100">
+            </el-table-column>
+
+            <el-table-column
+            prop="del"
+            label="del"
+            width="100">
+            </el-table-column>
+
+            <el-table-column
+            prop="dislike"
+            label="dislike"
+            width="100">
+            </el-table-column>
+
+            <el-table-column
+              prop=""
+              label="button"
+              width="100">
+              <button type="button"> 상세정보 </button>
+                <template slot-scope="scope">
+                    <router-link :to="'/meminfo/'+scope.row.memberId">  <!--memberid가 파라미터로 전달됨 -->
+                        <el-button type="primary" size="small" icon="el-icon-search">
+                            상세정보
+                        </el-button>
+                    </router-link>
+                </template>
+
+            </el-table-column>
+
 
 
         </el-table>    
@@ -116,19 +119,19 @@ import 'element-ui/lib/theme-chalk/index.css';
 export default {
     data(){
         return {
-            showAllMember:[]
+            showAllMember:null
 
         }
     },
     created(){
 
-        axios.post("http://localhost:9000/AdminMemberSelectAll")
-        .then(res => {
-        console.log(res.data)
-        this.showAllMember=res.data;
-        console.log(this.showAllMember)
-        })
-
+        // axios.post("http://localhost:9000/AdminMemberSelectAll")
+        // .then(res => {
+        // console.log(res.data)
+        // this.showAllMember=res.data;
+        // console.log(this.showAllMember)
+        // })
+        this.showMemberList()
     },
 
 
@@ -136,13 +139,13 @@ export default {
 
         // axios data processing
 
-        // showTestResult(){
-        //     axios.get("http://localhost:9000/getTestString")
-        //         .then(res => {
-        //         console.log(JSON.stringify(res.data))
-        //         this.output_data=res.data;
-        //     })
-        // },
+        showMemberList(){
+            axios.post("http://localhost:9000/AdminMemberSelectAll")
+                .then(res => {
+                // console.log(JSON.stringify(res.data))
+                this.showAllMember=res.data;
+            })
+        },
 
     },
 //   mounted(){
