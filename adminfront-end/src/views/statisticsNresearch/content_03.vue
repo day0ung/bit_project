0fb77b357f5eceaa6f8cdee153be47c6b9183201
bit_jonghyun <goste390@gmail.com>
2020-05-01 20:50:16 +0900

    통계 페이지 작업중 statisticsNresearch8 추가작업 필요.

--- adminfront-end/src/views/statisticsNresearch/content_03.vue
+++ adminfront-end/src/views/statisticsNresearch/content_03.vue
@@ -1,24 +1,60 @@
 <template>
-    <div class="content02_view">
-        <h1> {{title}} member3333333333333333333333333333333333333333</h1>
-    </div>
+        <div class="small">
+            <line-chart :chart-data="datacollection"></line-chart>
+            <button @click="fillData()">Randomize</button>
+            <div class="content01_view">
+                <h1> {{title}} member333333333333333333</h1>
+            </div>
+        </div>
+
 </template>
 
 <script>
+import LineChart from '@/api/LineChart.js'
+
 export default {
-    data(){
-        return {
-            title:'statisticsNresearch3'
-        }
+    components: {
+      LineChart
     },
-    methods:{
-
-
+    data () {
+      return {
+        datacollection: null,
+        title:'statisticsNresearch1'
+      }
+    },
+    mounted () {
+      this.fillData()
+    },
+    methods: {
+      fillData () {
+        this.datacollection = {
+          labels: [this.getRandomInt(), this.getRandomInt()],
+          datasets: [
+            {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }, {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }
+          ]
+        }
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      }
     }
 
 }
 </script>
 
 <style>
+
+ .small {
+    max-width: 600px;
+    margin:  150px auto;
+  }
 
 </style>
---
+++ adminfront-end/src/views/statisticsNresearch/content_05.vue
@@ -0,0 +1,75 @@
+<template>
+        <div class="small">
+
+          <div v-loading="loading">
+
+            <div class="Chart">
+              <h1 style="text-align:center;">DoughnutchartExample</h1>
+              <doughnut/>
+            </div>
+
+          </div>
+
+
+            <button @click="fillData()">Randomize</button>
+            <div class="content01_view">
+                <h1> {{title}} member333333333333333333</h1>
+            </div>
+        </div>
+</template>
+
+<script>
+
+
+
+
+
+
+export default {
+    components:{
+       Doughnut
+
+    },
+      
+
+
+
+  data () {
+    return {
+      loading:false,
+      datacollection: null,
+      title:'statisticsNresearch1',
+      getRawData:'',
+      setGenderData:[],
+      SetMan:[],
+      setWoman:[],
+      
+
+    }
+  },
+    methods() {
+
+    },
+    mounted () {
+    this.renderChart({
+      labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+      datasets: [
+        {
+          backgroundColor: [
+            '#41B883',
+            '#E46651',
+            '#00D8FF',
+            '#DD1B16'
+          ],
+          data: [40, 20, 80, 10]
+        }
+      ]
+    }, {responsive: true, maintainAspectRatio: false})
+  }
+
+}
+</script>
+
+<style>
+
+</style>
