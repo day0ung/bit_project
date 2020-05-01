709402ad93210b7265a81a8c6feca4dcb36dfb28
bit_jonghyun <goste390@gmail.com>
2020-05-01 12:44:35 +0900

    establish AdminStatistics structure

---
+++ back-end/palette/src/main/java/com/palette/controller/Admin_StatisticsController.java
@@ -0,0 +1,32 @@
+package com.palette.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class Admin_StatisticsController {
+    // @Autowired
+    // adminMemberService adminservice;
+
+    // @PostMapping(value = "/changeAuthCompanyMember")
+    // public boolean changeAuthCompanyMemberStatus(MemberDto dto) {
+    // 	System.out.println("changeAuthCompanyMemberStatus Method execute Now..");
+    	
+    // 	boolean isS = adminCompanyService.changeAUTHforCompanyMember(dto);
+    // 	System.out.println("changeAuthCompanyMemberStatus RESULT : \n" + isS );
+    // 	return isS;
+    // }
+    
+    
+
+
+
+}//end of  Admin_StatisticsController class
+
+
+    
+    
+
+
---
+++ back-end/palette/src/main/java/com/palette/service/AdminStatisticsService.java
@@ -0,0 +1,26 @@
+package com.palette.service;
+
+import java.util.List;
+
+import com.palette.dao.AdminStatisticsDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@Service
+@Transactional
+public class AdminStatisticsService {
+	@Autowired
+	AdminStatisticsDao adminStaticsdao;
+	
+
+
+
+	
+	
+	
+
+}// end of AdminStatisticsService class
